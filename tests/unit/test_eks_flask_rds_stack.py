940ffb83ef099ed0cdd1170f41efc3348e93d33c
takashi <yagita.takashi@gmail.com>
2022-04-18 23:25:05 +0900

    Initial commit

---
+++ tests/unit/test_eks_flask_rds_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from eks_flask_rds.eks_flask_rds_stack import EksFlaskRdsStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in eks_flask_rds/eks_flask_rds_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = EksFlaskRdsStack(app, "eks-flask-rds")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
